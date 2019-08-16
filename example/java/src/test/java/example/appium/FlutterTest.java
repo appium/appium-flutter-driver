24e758b3969017245d6f665ee2211f6c80d6ec2c
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-08-16 17:23:19 -0200

    dummy example

---
+++ example/java/src/test/java/example/appium/FlutterTest.java
@@ -0,0 +1,16 @@
+package example.appium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlutterTest extends BaseDriver{
+    @Test
+    public void simpleTest(){
+    }
+
+}
