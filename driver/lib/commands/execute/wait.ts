2ca9190158b0059edc03fa75c73b81c73ea507f3
Shibu Panda <shibu.prasad@sstsinc.com>
2020-02-03 17:12:41 +0000

    Added duration in wait methods and fixes for kotlin finder issue

--- driver/lib/commands/execute/wait.ts
+++ driver/lib/commands/execute/wait.ts
@@ -3,9 +3,38 @@ import { FlutterDriver } from '../../driver';
 export const waitForAbsent = async (
   self: FlutterDriver,
   elementBase64: string,
-) => await self.executeElementCommand(`waitForAbsent`, elementBase64);
+  durationMilliseconds?: number
+) => {
+
+  if(typeof durationMilliseconds == `undefined`){
+    durationMilliseconds = 10
+  }
+
+  if(typeof durationMilliseconds !== `number`){
+    // @todo BaseDriver's errors.InvalidArgumentError();
+    throw new Error(`durationMilliseconds is not a valid options`);
+  }
+
+  await self.executeElementCommand(`waitForAbsent`, elementBase64, {
+    timeout: durationMilliseconds * 1000,
+  });
+}; 
 
 export const waitFor = async (
   self: FlutterDriver,
   elementBase64: string,
-) => await self.executeElementCommand(`waitFor`, elementBase64);
+  durationMilliseconds?: number
+) => {
+  if(typeof durationMilliseconds == `undefined`){
+    durationMilliseconds = 10
+  }
+
+  if(typeof durationMilliseconds !== `number`){
+    // @todo BaseDriver's errors.InvalidArgumentError();
+    throw new Error(`durationMilliseconds is not a valid options`);
+  }
+
+  await self.executeElementCommand(`waitFor`, elementBase64, {
+    timeout: durationMilliseconds * 1000,
+  });
+};
