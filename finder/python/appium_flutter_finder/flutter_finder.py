72d01d6dd90cde1fb32c4ec388264ebd9aa3a0ca
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-02-07 01:49:53 +0900

    add other finders

--- finder/python/appium_flutter_finder/flutter_finder.py
+++ finder/python/appium_flutter_finder/flutter_finder.py
@@ -11,11 +11,31 @@ class FlutterElement(WebElement):
             driver, element_id, w3c=True
         )
 
-    # text_finder = by_type('type')
-    # element = FlutterElement(driver, text_finder)
-
 
 class FlutterFinder(object):
+    def by_ancestor(self, serialized_finder, matching, match_root=False):
+        return self._by_ancestor_or_descendant(
+            type_='Ancestor',
+            serialized_finder=serialized_finder,
+            matching=matching,
+            match_root=match_root
+        )
+
+    def by_descendant(self, serialized_finder, matching, match_root=False):
+        return self._by_ancestor_or_descendant(
+            type_='Descendant',
+            serialized_finder=serialized_finder,
+            matching=matching,
+            match_root=match_root
+        )
+
+    def by_semantics_label(self, label, isRegExp=False):
+        return self._serialize(dict(
+            finderType='BySemanticsLabel',
+            isRegExp=isRegExp,
+            label=label
+        ))
+
     def by_tooltip(self, text):
         return self._serialize(dict(
             finderType='ByTooltipMessage',
@@ -49,6 +69,27 @@ class FlutterFinder(object):
     def _serialize(self, finder_dict):
         return base64.b64encode(json.dumps(finder_dict))
 
+    def _by_ancestor_or_descendant(self, type_, serialized_finder, matching, match_root=False):
+        param = dict(finderType=type_, matchRoot=match_root)
+
+        try:
+            finder = json.dumps(base64.b64decode(serialized_finder))
+        except:
+            finder = dict()
+        for finder_key, finder_value in finder.items():
+            param['of_{}'.format(finder_key)] = finder_value
+
+        try:
+            matching = json.dumps(base64.b64decode(matching))
+        except:
+            matching = dict()
+        for matching_key, matching_value in matching.items():
+            param['matching_{}'.format(matching_key)] = matching_value
+
+        return self._serialize(param)
+
+
+# Example
 
 driver = Remote('http://localhost:4723/wd/hub', dict(
     platformName='iOS',
