c5b66c5627f8c0e9b40918a46dadaf086f1038a2
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2020-02-10 11:59:40 +0700

    Added duration for wait command (#52)
    Close #30. This is part of original #46 by @shibupanda

--- driver/lib/commands/execute/wait.ts
+++ driver/lib/commands/execute/wait.ts
@@ -1,11 +1,25 @@
 import { FlutterDriver } from '../../driver';
 
-export const waitForAbsent = async (
+const waitForConstructor = (command: `waitForAbsent` | `waitFor`) => async (
   self: FlutterDriver,
   elementBase64: string,
-) => await self.executeElementCommand(`waitForAbsent`, elementBase64);
+  durationMilliseconds?: number,
+) => {
 
-export const waitFor = async (
-  self: FlutterDriver,
-  elementBase64: string,
-) => await self.executeElementCommand(`waitFor`, elementBase64);
+  let args = {};
+
+  if (typeof durationMilliseconds === `number`) {
+    args = {
+      timeout: durationMilliseconds * 1000,
+    };
+  } else if (typeof durationMilliseconds !== `undefined`) {
+    // @todo BaseDriver's errors.InvalidArgumentError();
+    throw new Error(`durationMilliseconds is not a valid options`);
+  }
+
+  await self.executeElementCommand(command, elementBase64, args);
+};
+
+export const waitForAbsent = waitForConstructor(`waitForAbsent`);
+
+export const waitFor = waitForConstructor(`waitFor`);
