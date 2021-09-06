b6212c37b81be07931aa626c6daca8c4cf6830ce
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-09-06 00:00:51 -0700

    chore: bump version in python finder (#186)

--- finder/python/setup.py
+++ finder/python/setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Appium-Flutter-Finder',
-    version='0.1.5',
+    version='0.2.0',
     description='An extention of finder for Appium flutter',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -23,13 +23,12 @@ setup(
     packages=find_packages(include=['appium_flutter_finder*']),
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Environment :: Console',
         'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',
