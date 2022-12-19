6851fe835954a9ae84aa34861f39a1c7a7af00d8
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-12-19 01:53:38 -0800

    feat: always call set/get clipboard from the base driver

---
+++ driver/lib/commands/clipboard.ts
@@ -0,0 +1,22 @@
+import { FlutterDriver } from '../driver';
+
+/**
+ * Set clipboard content via each native app driver
+ * @param this the FlutterDriver
+ * @param content the content to get the clipboard
+ * @param contentType the contentType to set the data type
+ */
+export const setClipboard = async function(this: FlutterDriver, content: string, contentType: string) {
+  await this.proxydriver.setClipboard(content, contentType);
+};
+
+
+/**
+ * Get the clipboard content via each native app driver
+ * @param this the FlutterDriver
+ * @param contentType the contentType to set the data type
+ */
+
+export const getClipboard = async function(this: FlutterDriver, contentType: string) {
+  await this.proxydriver.getClipboard(contentType);
+};
