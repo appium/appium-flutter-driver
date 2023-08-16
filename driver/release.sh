7b29945fd2bde027ec7c40415a7a45d51496b4fc
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-08-16 01:15:57 -0700

    tune release script

--- driver/release.sh
+++ driver/release.sh
@@ -3,6 +3,7 @@
 echo "refreshing dependencies"
 rm npm-shrinkwrap.json
 APPIUM_SKIP_CHROMEDRIVER_INSTALL=1 npm run clean-dependency
+npm install --production
 npm prune --production
 rm -rf node_modules/appium
 npm shrinkwrap
