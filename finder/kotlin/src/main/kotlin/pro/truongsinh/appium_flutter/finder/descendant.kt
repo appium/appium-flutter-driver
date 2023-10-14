77a9deabc468918eb6c0df427cb02b435752ebc8
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-10-14 02:15:37 -0700

    modify kotlin

--- finder/kotlin/src/main/kotlin/pro/truongsinh/appium_flutter/finder/descendant.kt
+++ finder/kotlin/src/main/kotlin/pro/truongsinh/appium_flutter/finder/descendant.kt
@@ -3,10 +3,10 @@
 package pro.truongsinh.appium_flutter.finder
 
 fun descendant(of: FlutterElement, matching: FlutterElement, matchRoot: Boolean = false, firstMatchOnly: Boolean = false): FlutterElement {
-  val m = mutableMapOf(
+  val m = mutableMapOf<String, Any>(
     "finderType" to "Descendant",
-    "matchRoot" to matchRoot,
-    "firstMatchOnly" to firstMatchOnly
+    "matchRoot" to matchRoot.toString(),
+    "firstMatchOnly" to firstMatchOnly.toString()
   )
   m["of"] = of.getRawMap()
   m["matching"] = matching.getRawMap()
