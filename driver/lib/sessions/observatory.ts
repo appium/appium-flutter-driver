b346734d7c092255fbfa0c862e5cc61cd139c05c
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-11-24 10:32:49 -0800

    remove

--- driver/lib/sessions/observatory.ts
+++ driver/lib/sessions/observatory.ts
@@ -227,9 +227,7 @@ export const fetchObservatoryUrl = (deviceLogs: [{ message: string }]): URL => {
       break;
     }
   }
-  if (dartObservatoryURL) {
-    log.info(`${dartObservatoryURL.href} was found.`);
-  } else {
+  if (!dartObservatoryURL) {
     throw new Error(`No observatory URL matching to '${OBSERVATORY_URL_PATTERN}' was found in the device log. ` +
       `Please make sure the application under test is configured properly according to ` +
       `https://github.com/appium-userland/appium-flutter-driver#usage and that it does not crash on startup.`);
