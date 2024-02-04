10d5a7b7b57af004fad5096ca1bbc026798cb4c4
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-02-03 20:59:53 -0800

    chore: remove appium-android-driver as no references (#656)
    * chore: remove appium-android-driver as no references
    
    * update release.sh

--- driver/release.sh
+++ driver/release.sh
@@ -6,9 +6,9 @@ APPIUM_SKIP_CHROMEDRIVER_INSTALL=1 npm run clean-dependency
 npm install --production
 npm prune --omit=dev --omit=peer
 rm -rf node_modules/appium
-npm shrinkwrap
+npm shrinkwrap --omit=dev --omit=peer
 
 # to install types again
-npm install
+npm install --only=dev --no-package-lock
 
 echo "complete the refreshment"
