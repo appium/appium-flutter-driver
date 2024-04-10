4e426ec6eee57e1ac4e93340ce78ba0d2f02f389
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-04-09 22:35:57 -0700

    chore: add env var to control the checks

--- driver/lib/doctor/checks.js
+++ driver/lib/doctor/checks.js
@@ -1,21 +1,41 @@
-import { doctor as androidDoctor } from "appium-android-driver";
-import { doctor as iosDoctor } from "appium-xcuitest-driver";
+import { doctor as androidDoctor } from 'appium-android-driver';
+import { doctor as iosDoctor } from 'appium-xcuitest-driver';
 
-
-export const androidHomeCheck = androidDoctor.androidHomeCheck;
-export const javaHomeCheck = androidDoctor.javaHomeCheck;
-export const javaHomeValueCheck = androidDoctor.javaHomeValueCheck;
-export const androidSdkCheck = androidDoctor.androidSdkCheck;
-export const optionalBundletoolCheck = androidDoctor.optionalBundletoolCheck;
-export const optionalGstreamerCheck = androidDoctor.optionalGstreamerCheck;
-
-export const xcodeCheck = iosDoctor.required.xcodeCheck;
-export const xcodeToolsCheck = iosDoctor.required.xcodeToolsCheck;
-export const envVarAndPathCheck = iosDoctor.required.envVarAndPathCheck;
+// shared
 export const homeEnvVarCheck = iosDoctor.required.homeEnvVarCheck;
-export const optionalIdbCommandCheck = iosDoctor.optional.optionalIdbCommandCheck;
-export const optionalApplesimutilsCommandCheck = iosDoctor.optional.optionalApplesimutilsCheck;
 
+let androidHomeCheck;
+let javaHomeCheck;
+let javaHomeValueCheck;
+let androidSdkCheck;
+let optionalBundletoolCheck;
+let optionalGstreamerCheck;
+if (!process.env.SKIP_ANDROID) {
+  androidHomeCheck = androidDoctor.androidHomeCheck;
+  javaHomeCheck = androidDoctor.javaHomeCheck;
+  javaHomeValueCheck = androidDoctor.javaHomeValueCheck;
+  androidSdkCheck = androidDoctor.androidSdkCheck;
+  optionalBundletoolCheck = androidDoctor.optionalBundletoolCheck;
+  optionalGstreamerCheck = androidDoctor.optionalGstreamerCheck;
+}
+
+let xcodeCheck;
+let xcodeToolsCheck;
+let envVarAndPathCheck;
+let optionalIdbCommandCheck;
+let optionalApplesimutilsCommandCheck;
+if (!process.env.SKIP_IOS) {
+  xcodeCheck = iosDoctor.required.xcodeCheck;
+  xcodeToolsCheck = iosDoctor.required.xcodeToolsCheck;
+  envVarAndPathCheck = iosDoctor.required.envVarAndPathCheck;
+  optionalIdbCommandCheck = iosDoctor.optional.optionalIdbCommandCheck;
+  optionalApplesimutilsCommandCheck = iosDoctor.optional.optionalApplesimutilsCheck;
+}
 
 // shared
 export const optionalFfmpegCheck = androidDoctor.optionalFfmpegCheck;
+
+export {
+  androidHomeCheck, javaHomeCheck, javaHomeValueCheck, androidSdkCheck, optionalBundletoolCheck, optionalGstreamerCheck,
+  xcodeCheck, xcodeToolsCheck, envVarAndPathCheck, optionalIdbCommandCheck, optionalApplesimutilsCommandCheck
+};
