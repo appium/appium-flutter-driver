a5a44108f41806f02c86ba64f8b3d7c0eca120fd
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-07-13 22:44:01 -0700

    chore(kotlin): remove dokka for now (#716)
    * remove unused plugin
    
    * chore(kotlin): remove dokka for now

--- finder/kotlin/build.gradle.kts
+++ finder/kotlin/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     id("kotlinx-serialization") version "1.3.40"
     `maven-publish`
     kotlin("jvm") version "1.3.40" 
-    id("org.jetbrains.dokka") version "1.6.0"
 }
 
 repositories {
@@ -22,22 +21,10 @@ dependencies {
     testImplementation("junit:junit:4.12")
 }
 
-tasks.dokkaHtml.configure {
-    outputDirectory.set(buildDir.resolve("dokka"))
-}
-
-val dokkaJar by tasks.creating(Jar::class) { 
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles Kotlin docs with Dokka"
-    classifier = "javadoc"
-    from(tasks.dokkaHtml)
-}
-
 publishing {
     publications {
         create<MavenPublication>("default") { 
             from(components["java"])
-            artifact(dokkaJar) 
         }
     }
     repositories {
