6661437532a98cb4a11c5a6cab7d0b46d1204a51
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-02-22 11:48:09 -0800

    chore: fix lint and add ignore for build

--- driver/lib/sessions/base64url.ts
+++ driver/lib/sessions/base64url.ts
@@ -1,14 +1,15 @@
 import _ from 'lodash';
 import { util } from '@appium/support';
 
-export const decode = (input: string | {'ELEMENT': string} | {[util.W3C_WEB_ELEMENT_IDENTIFIER]: string}): string => {
+export const decode = (input: string | {ELEMENT: string} | {[util.W3C_WEB_ELEMENT_IDENTIFIER]: string}): string => {
   let base64String: string = ``;
   if (_.isString(input)) {
     base64String = input as string;
   } else if (_.has(input, util.W3C_WEB_ELEMENT_IDENTIFIER)) {
     base64String = input[util.W3C_WEB_ELEMENT_IDENTIFIER] as string;
   } else if (_.has(input, 'ELEMENT')) {
-    base64String = input['ELEMENT'] as string;
+    // @ts-ignore TS2339
+    base64String = input.ELEMENT as string;
   } else {
     throw new Error(
       `Input is is expceted to be a base64-encoded string or a valid element object. ` +
