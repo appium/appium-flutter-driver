7a5c5aecb900998c2b9343cacee287a93e74ee45
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-01-28 00:40:51 -0800

    fix: lint (#848)

--- driver/lib/commands/element.ts
+++ driver/lib/commands/element.ts
@@ -1,6 +1,6 @@
 import { FlutterDriver } from '../driver';
 
-export const getText = async function(this: FlutterDriver, el: string): Promise<string|null> {
+export const getText = async function(this: FlutterDriver, el: string): Promise<string | null> {
   const response = await this.executeElementCommand(`get_text`, el);
   return response.text;
 };
