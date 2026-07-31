13bac39c06d0bb8f4fe93c556e1234146b2eb5ca
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2026-08-01 08:20:08 +0900

    chore: format

--- finder/nodejs/lib/base64url_test.ts
+++ finder/nodejs/lib/base64url_test.ts
@@ -1,5 +1,6 @@
 import assert from 'node:assert/strict';
 import {describe, it} from 'node:test';
+
 import {decode, encode} from './base64url.js';
 
 void describe(`base64url`, () => {
--- finder/nodejs/lib/serializer_test.ts
+++ finder/nodejs/lib/serializer_test.ts
@@ -1,5 +1,6 @@
 import assert from 'node:assert/strict';
 import {describe, it} from 'node:test';
+
 import * as find from './serializer.js';
 
 void describe(`serializer`, () => {
